95b587f9880183af911f29f3ba7e82ab5cc6379f
Paul Silcock <ps88uk@gmail.com>
2022-12-12 23:37:50 +0000

    feat: tweaks blog preview borders

--- components/post.tsx
+++ components/post.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const PostPreview: React.FC<Props> = ({ title, slug, className }) => (
   <Link href="/" className={`flex flex-col w-full md:w-56 h-36 md:h-60 rounded-xl ${className ?? ""}`}>
-    <div className="p-3 m-[0.125rem] flex-1 rounded-xl bg-base-100">
+    <div className="p-3 m-1 flex-1 rounded-xl bg-base-100">
       <h4>{title}</h4>
       <p>{slug}</p>
     </div>
